8ade1fd2a660037ddb9a7bcf476081f2178b9d1d
preethydp <preethy.pichala@iwedia.com>
2025-08-21 09:50:19 +0530

    Added price to dish detail

--- lib/ui/widgets/menu/dish_detail.dart
+++ lib/ui/widgets/menu/dish_detail.dart
@@ -28,158 +28,158 @@ class DishDetail extends ConsumerWidget {
         ),
       ),
       padding: const EdgeInsets.only(left: 20),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (dish!.dish_image.isNotEmpty)
-              Image.network(
-                dish!.dish_image,
-                width: double.infinity,
-                height: 220,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Image.asset(
-                    'assets/images/default_dish.png',
-                    width: double.infinity,
-                    height: 220,
-                    fit: BoxFit.cover,
-                  );
-                },
-              ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                VegIndicator(
-                    size: 6,
-                    color: (dish!.dish_type.toLowerCase() == 'veg'
-                        ? Colors.green
-                        : Colors.red)),
-                const SizedBox(width: 6),
-                Text(
-                  categoryName,
-                  style: const TextStyle(
-                    color: Colors.white70,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (dish!.dish_image.isNotEmpty)
+            Image.network(
+              dish!.dish_image,
+              width: double.infinity,
+              height: 220,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Image.asset(
+                  'assets/images/default_dish.png',
+                  width: double.infinity,
+                  height: 220,
+                  fit: BoxFit.cover,
+                );
+              },
             ),
-            Text(
-              dish!.name,
-              style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              dish!.description,
-              maxLines: 2,
-              style: const TextStyle(color: Colors.white70),
-            ),
-            const SizedBox(height: 2),
-            if (dish!.cooking_request?.isNotEmpty == true)
+          const SizedBox(height: 8),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              VegIndicator(
+                  size: 6,
+                  color: (dish!.dish_type.toLowerCase() == 'veg'
+                      ? Colors.green
+                      : Colors.red)),
+              const SizedBox(width: 6),
               Text(
-                'Cooking instruction : ${dish!.cooking_request}',
-                maxLines: 1,
-                style: const TextStyle(color: Colors.white54),
+                categoryName,
+                style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            const SizedBox(height: 6),
-            Center(
-              child: SizedBox(
-                height: 35,
-                child: Focus(
-                  onKeyEvent: (node, event) {
-                    if (event is! KeyDownEvent) return KeyEventResult.ignored;
-                    if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-                      if (ref.watch(showCategoriesProvider)) {
-                        final lastFocusedCategory =
-                            ref.read(selectedCategoryProvider);
-                        final categoryIndex = (lastFocusedCategory >= 0)
-                            ? lastFocusedCategory
-                            : 0;
+            ],
+          ),
+          Text(
+            '₹${dish!.dish_price}',
+            style: const TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            dish!.name,
+            style: const TextStyle(
+                fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            dish!.description,
+            maxLines: 2,
+            style: const TextStyle(color: Colors.white70),
+          ),
+          const SizedBox(height: 2),
+          if (dish!.cooking_request?.isNotEmpty == true)
+            Text(
+              'Cooking instruction : ${dish!.cooking_request}',
+              maxLines: 1,
+              style: const TextStyle(color: Colors.white54),
+            ),
+          const SizedBox(height: 6),
+          Center(
+            child: SizedBox(
+              height: 35,
+              child: Focus(
+                onKeyEvent: (node, event) {
+                  if (event is! KeyDownEvent) return KeyEventResult.ignored;
+                  if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+                    if (ref.watch(showCategoriesProvider)) {
+                      final lastFocusedCategory =
+                          ref.read(selectedCategoryProvider);
+                      final categoryIndex =
+                          (lastFocusedCategory >= 0) ? lastFocusedCategory : 0;
 
-                        ref
-                            .read(categoryFocusNodeProvider(categoryIndex))
-                            .requestFocus();
-                      } else {
-                        final lastFocusedDish = ref.read(focusedDishProvider);
-                        int index = 0;
-                        if (lastFocusedDish >= 0) {
-                          index = lastFocusedDish;
-                        }
-                        ref.read(dishFocusNodeProvider(index)).requestFocus();
+                      ref
+                          .read(categoryFocusNodeProvider(categoryIndex))
+                          .requestFocus();
+                    } else {
+                      final lastFocusedDish = ref.read(focusedDishProvider);
+                      int index = 0;
+                      if (lastFocusedDish >= 0) {
+                        index = lastFocusedDish;
                       }
-                      return KeyEventResult.handled;
+                      ref.read(dishFocusNodeProvider(index)).requestFocus();
                     }
-                    return KeyEventResult.ignored;
+                    return KeyEventResult.handled;
+                  }
+                  return KeyEventResult.ignored;
+                },
+                child: ElevatedButton(
+                  focusNode: ref.watch(cookingInstructionFocusNodeProvider),
+                  onPressed: () {
+                    showDialog(
+                      barrierColor: Colors.black87,
+                      context: context,
+                      builder: (context) {
+                        TextEditingController instructionController =
+                            TextEditingController();
+                        return CookingInstructionDialog(
+                          dishName: dish!.name,
+                          controller: instructionController,
+                          onSave: (text) {
+                            ref
+                                .read(mealsProvider.notifier)
+                                .updateDishCookingInstruction(dish!.id, text);
+                            Navigator.of(context).pop();
+                          },
+                          onCancel: () {
+                            Navigator.of(context).pop();
+                          },
+                        );
+                      },
+                    );
                   },
-                  child: ElevatedButton(
-                    focusNode: ref.watch(cookingInstructionFocusNodeProvider),
-                    onPressed: () {
-                      showDialog(
-                        barrierColor: Colors.black87,
-                        context: context,
-                        builder: (context) {
-                          TextEditingController instructionController =
-                              TextEditingController();
-                          return CookingInstructionDialog(
-                            dishName: dish!.name,
-                            controller: instructionController,
-                            onSave: (text) {
-                              ref
-                                  .read(mealsProvider.notifier)
-                                  .updateDishCookingInstruction(dish!.id, text);
-                              Navigator.of(context).pop();
-                            },
-                            onCancel: () {
-                              Navigator.of(context).pop();
-                            },
-                          );
-                        },
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (states) {
-                          if (states.contains(WidgetState.focused)) {
-                            return Theme.of(context).colorScheme.primary;
-                          }
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                      (states) {
+                        if (states.contains(WidgetState.focused)) {
+                          return Theme.of(context).colorScheme.primary;
+                        }
+                        return Colors.white70;
+                      },
+                    ),
+                    foregroundColor: WidgetStateProperty.resolveWith<Color>(
+                      (states) {
+                        if (states.contains(WidgetState.focused)) {
                           return Colors.white70;
-                        },
-                      ),
-                      foregroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (states) {
-                          if (states.contains(WidgetState.focused)) {
-                            return Colors.white70;
-                          }
-                          return Colors.black;
-                        },
-                      ),
-                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
+                        }
+                        return Colors.black;
+                      },
+                    ),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    child: Text(
-                      (dish!.cooking_request == null ||
-                              dish!.cooking_request!.isEmpty)
-                          ? "Add Cooking Instructions"
-                          : "Edit Cooking Instructions",
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  child: Text(
+                    (dish!.cooking_request == null ||
+                            dish!.cooking_request!.isEmpty)
+                        ? "Add Cooking Instructions"
+                        : "Edit Cooking Instructions",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
